b28feb03612d57b02b71ee3c38c1e3dd86f1f7e4
“Camila <camilacarvalho@sou.faccat.br>
2020-06-30 18:05:04 -0300

    Implementado as demais classes, métodos e feito update para workbench

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,24 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); //aqui declaramos uma coleção do tipo vendedores e instanciamos com uma lista de vendedores
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {//retorna a soma da lista de vendedores, para a soma foi passado o método que soma as vendas de cada vendedor na classe vendedor
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
